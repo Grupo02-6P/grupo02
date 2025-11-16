afb56bfbe089902fda6585bc94a14fad7a6b59b4
mayumihb <mayumihb80@gmail.com>
2025-11-16 13:53:55 -0300

    fix: change UsersService import as providers to UsersModule as imports

--- contabil-backend/src/entry/entry.module.ts
+++ contabil-backend/src/entry/entry.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EntryService } from './entry.service';
 import { EntryController } from './entry.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [EntryController],
   providers: [EntryService],
 })
--- contabil-backend/src/type-entry/type-entry.module.ts
+++ contabil-backend/src/type-entry/type-entry.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeEntryService } from './type-entry.service';
 import { TypeEntryController } from './type-entry.controller';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [TypeEntryController],
-  providers: [TypeEntryService, UsersService],
+  providers: [TypeEntryService],
 })
 export class TypeEntryModule {}
